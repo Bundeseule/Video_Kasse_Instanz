c8ccdc2ba260ee5621abb5c200fd7978900255e7
Bundeseule <lordwhity@gmail.com>
2021-06-30 12:02:08 +0200

    soweit alles implementiert, wie es brauchbar ist fuer das video isntace

--- Video_Kasse_Instanz/Form1.cs
+++ Video_Kasse_Instanz/Form1.cs
@@ -16,5 +16,26 @@ namespace Video_Kasse_Instanz
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Student
+        {
+            private string name;
+
+            public Student()
+            {
+                MessageBox.Show("Konstruktor der Klasse Student wird aufgerufen");
+                name = "Hans";
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+        }
     }
 }
